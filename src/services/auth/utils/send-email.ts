032781aaf4a0c40f55826c4db654ab08b663b5d8
Lucas Da Silva <lucas.info011@gmail.com>
2025-05-12 11:20:40 -0300

    feat:implementa envio de email na redefinicao da senha

---
+++ src/services/auth/utils/send-email.ts
@@ -0,0 +1,18 @@
+import transporter from "@/configs/nodemailer";
+import { env } from "@/env";
+import path from "path";
+import fs from "fs";
+
+export default async function sendEmail(email: string, token: string) {
+  const templatePath = path.join(__dirname,'../../../', 'templates','resetPassword.html');
+  const template = fs.readFileSync(templatePath, 'utf-8');
+
+  const html = template.replace("{{TOKEN}}", token);
+
+  const info = await transporter.sendMail({ 
+    from: env.EMAIL_USER,
+    to: email,
+    subject: "Redefinição de Senha - Ciclo Zero",
+    html,
+  });
+}
